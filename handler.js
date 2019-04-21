ff72f6d909a29abc5f967d01e1590bb1bd6fac4a
Michael Dimmock <michael.dimmock@virgin.net>
2019-04-21 10:41:09 +0100

    updated packages and runtime, removed data deadlink

--- handler.js
+++ handler.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 
 const urls = [
   'http://vatsim-data.hardern.net/vatsim-data.txt',
-  'http://vatsim.aircharts.org/vatsim-data.txt',
   'http://data.vattastic.com/vatsim-data.txt',
   'http://info.vroute.net/vatsim-data.txt',
 ];
